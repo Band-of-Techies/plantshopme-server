ac78550fc291c8122303617153a668ac74cffbd8
Rahul C V <rahulcv447@gmail.com>
2023-12-14 14:45:59 +0530

    updated

--- db.js
+++ db.js
@@ -8,8 +8,10 @@ module.exports = () => {
 		useUnifiedTopology: true,
 	};
 	try {
-		mongoose.connect(process.env.DB, connectionParams);
-		console.log("Connected to database successfully");
+		
+mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log("Connected to database successfully");
+
 	} catch (error) {
 		console.log(error);
 		console.log("Could not connect database!");
